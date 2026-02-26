d5bfb054feaadf9d5f854ee25e8a677b14b3033a
Wide <lemastaweshaye@gmail.com>
2026-02-26 17:59:50 -0600

    First update

--- rps.py
+++ rps.py
@@ -1,2 +1,34 @@
-#create rock paper scissors game
-#3 options: rock, paper, scissors
+import random
+from colorama import init, Fore
+init(autoreset=True)
+
+choices = ["rock", "paper", "scissors"]
+
+print("Welcome to Rock Paper Scissors!")
+
+while True:
+    
+    player = input("Choose rock, paper, or scissors: ").lower()
+
+    computer = random.choice(choices)
+    print("Computer chose:", computer)
+
+    if player == "rock" and computer == "scissors":
+        print("You win rock star!")
+    elif player == computer:
+        print("It's a tie!")
+    elif player == "scissors" and computer == "paper":
+        print("You are really cutting down your opponent! You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win! You are a paper champion!")
+    else:
+        print("You lose! Better luck next time!")
+
+    again = input("Do you want to play again? (yes/no): ").lower()
+
+    if again != "yes":
+        print("Thanks for playing!")
+        break
+
+        
+
